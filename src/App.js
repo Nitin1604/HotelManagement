054b9540491934b1c122d9ef888d571aa97e7971
Nitin1604 <nitinsokhal@gmail.com>
2023-03-31 22:05:32 +0530

    basic example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(100);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
-      <h1>This is main app</h1>
+      <h1>This is value of count {count}</h1>
+      <button onClick={()=>{incrementCount()}}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+
     </div>
   );
 }
