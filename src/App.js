066fc7560be9b081e3c384b40263c31634ef4fbb
Nitin1604 <nitinsokhal@gmail.com>
2023-04-01 23:50:22 +0530

    App.js code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       {/* Navbar block start here */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">Hotel Management</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -18,7 +18,7 @@ function App() {
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link active" aria-current="page" href="#">About</a>
               </li>
             </ul>
           </div>
@@ -43,6 +43,7 @@ function App() {
           </div>
           {/* Name block will ends here */}
 
+          {/* Contact block will start here */}
           <div className='row'>
             <div className="col-sm-4">
               <div className="form-group">
@@ -51,7 +52,7 @@ function App() {
               </div>
             </div>
           </div>
-
+          {/* Contact block will ends here */}
 
           {/* Email Address block start here */}
           <div className='row'>
