b1743f0248bc5d36253e99d75d1d46ee5bcfbe86
Nitin1604 <nitinsokhal@gmail.com>
2023-04-01 18:09:00 +0530

    uploading new content of App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// Import your App.css file here in App.js
 import './App.css';
-import { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(100);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  }
-
-  const decrementCount = () => {
-    setCount(count - 1);
-  }
-
   return (
     <div className="App">
-      <h1>This is value of count {count}</h1>
-      <button onClick={()=>{incrementCount()}}>Increment</button>
-      <button onClick={decrementCount}>Decrement</button>
+      <div className='container my-5 offset-sm-4'>
+      {/* form will start here */}
+        <form>
+          <h2>Customer's Login Page</h2>
+          {/* Email Address block start here */}
+          <div className='row'>
+            <div className="col-sm-4">
+              <div className="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+            </div>
+          </div>
+          {/* Email Address block start here */}
 
+          {/* Password block start here */}
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+              </div>
+            </div>
+          </div>
+          {/* Password block start here */}
+
+        
+          {/* Submit Button block start here */}
+          <div className='my-2'>
+          <button type="submit" className="btn  btn-danger">Submit</button>
+          </div>
+          {/* Submit Button block start here */}
+        </form>
+
+      {/* form will ends here */}
+
+      </div> 
 
     </div>
   );
