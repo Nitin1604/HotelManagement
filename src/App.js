fb7cf16a38ed54459dd5414cdbdcb270e0c63673
Nitin1604 <nitinsokhal@gmail.com>
2023-04-01 21:03:05 +0530

    App.js code

--- src/App.js
+++ src/App.js
@@ -5,8 +5,33 @@ function App() {
 
   return (
     <div className="App">
+      {/* Navbar block start here */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+      {/* Navbar block ends here */}
+
       <div className='container my-5 offset-sm-4'>
-      {/* form will start here */}
+
+        {/* form will start here */}
         <form>
           <h2>Customer's Login Page</h2>
 
@@ -15,7 +40,7 @@ function App() {
             <div className="col-sm-4">
               <div className="form-group">
                 <label for="exampleInputName">Name</label>
-                <input className="form-control" id="exampleInputName" placeholder="Enter your name"/>
+                <input className="form-control" id="exampleInputName" placeholder="Enter your name" />
               </div>
             </div>
           </div>
@@ -25,7 +50,7 @@ function App() {
             <div className="col-sm-4">
               <div className="form-group">
                 <label for="exampleInputContact">Contact No</label>
-                <input className="form-control" id="exampleInputContact" placeholder="Enter your contact number"/>
+                <input className="form-control" id="exampleInputContact" placeholder="Enter your contact number" />
               </div>
             </div>
           </div>
@@ -55,17 +80,17 @@ function App() {
           </div>
           {/* Password block ends here */}
 
-        
+
           {/* Submit Button block start here */}
           <div className='my-2'>
-          <button type="submit" className="btn  btn-danger">Submit</button>
+            <button type="submit" className="btn  btn-danger">Submit</button>
           </div>
           {/* Submit Button block ends here */}
         </form>
+        {/* form will ends here */}
 
-      {/* form will ends here */}
 
-      </div> 
+      </div>
 
     </div>
   );
