8ce3fda3fd35c80889c2d5be1132ae3525619e21
Nitin1604 <nitinsokhal@gmail.com>
2023-04-01 18:32:23 +0530

    My code of App.js till now

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
               <div className="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                <small id="emailHelp" className="form-text text-muted">Your email id is save , we can't share it with anyone.</small>
               </div>
             </div>
           </div>
@@ -27,6 +27,7 @@ function App() {
               <div className="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <small id="passwordHelp" className="form-text text-muted">Your password is save , we can't share it with anyone.</small>
               </div>
             </div>
           </div>
