169d543caaadd70bac7f4d62d60fed30d25703b6
Nitin1604 <nitinsokhal@gmail.com>
2023-04-02 23:47:46 +0530

    Code of home.js

--- src/Components/home.js
+++ src/Components/home.js
@@ -20,6 +20,21 @@ const HomeComponent = () => {
                     </div>
                 </div>
                 {/* card 1 block ends here */}
+                
+                {/* card 2 block start here */}
+                <div className="row">
+                    <div className="col-sm-2">
+                        <div className="card mx-2 my-4">
+                            <img className="card-img-top" src="..." alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* card 2 block ends here */}
             </div>
         </div>
 
