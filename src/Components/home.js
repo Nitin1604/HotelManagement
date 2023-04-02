f533a4efc3c57fb1f159e51bcf84cd30918d28b2
Nitin1604 <nitinsokhal@gmail.com>
2023-04-02 22:34:55 +0530

    added home.js

--- src/Components/home.js
+++ src/Components/home.js
@@ -6,9 +6,24 @@ const HomeComponent = () => {
         <div id="HomeComponent">
             <NavigationComponent />
             <div className="content">
-              <h1>This is homeComponent</h1>
+                {/* card 1 block start here */}
+                <div className="row">
+                    <div className="col-sm-2">
+                        <div className="card mx-2 my-4">
+                            <img className="card-img-top" src="..." alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1 block ends here */}
             </div>
         </div>
+
+
     )
 }
 
