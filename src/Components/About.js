81ff235fc0e887b5f0a3913170c94d74084e9657
Nitin1604 <nitinsokhal@gmail.com>
2023-04-05 08:32:54 +0530

    Code of About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,25 @@
 import NavigationComponent from "./Navigation";
-
+import hotel1 from './hotel1.jpg';
 const AboutComponent = () => {
 
     return (
+        <>
         <div id="AboutComponent">
             <NavigationComponent />
             <div className="content">
-                <h1>This is about Component</h1>
+            <div className="row my-3">
+                <div className="col-sm-12">
+                    <div className="row">
+                        <div className="col-sm-4 mx-2">
+                        <img src={hotel1} alt="hotel1" />
+                        
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+    </>
     )
 }
 
