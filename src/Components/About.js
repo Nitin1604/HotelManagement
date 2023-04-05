66798eb7618ce2bc0b003a280034e41e19872527
Nitin1604 <nitinsokhal@gmail.com>
2023-04-05 09:08:18 +0530

    Code of About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,25 +8,7 @@ const AboutComponent = () => {
             <div id="AboutComponent">
                 <NavigationComponent />
                 <div className="content">
-                    <div className="card-group">
-                        <div className="card">
-                            <img src={hotel1} alt="hotel1" />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src={hotel2} alt="hotel2" />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-
-                    </div>
+                    <h2>This is AboutComponent</h2>
                 </div>
             </div>
         </>
