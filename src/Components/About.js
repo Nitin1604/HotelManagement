322e8ebe796641c9a673f2a44714d6474869d7f8
Nitin1604 <nitinsokhal@gmail.com>
2023-05-25 13:47:23 +0530

    About.js code

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,25 +1,35 @@
 import NavigationComponent from "./Navigation";
+import hotel3 from './hotel3.jpg';
 const AboutComponent = () => {
 
     return (
         <>
             <div id="AboutComponent">
                 <NavigationComponent />
+                {/* content start here */}
                 <div className="content">
+                    {/* Row 1 */}
                     <div className="row">
                         <div className="col-sm-2">
                             <div className="card mx-2 my-2">
-                                <div className="card-body">
+                                {/* card-body will start here */}
+                                <div className="card-body1">
                                     <h5 className="card-title">Radisson Noida</h5>
-                                    <p>Radisson Noida Hotel has many facilities like Free Parking,
-                                        Free WiFi , Swimming Pool , Bath , Air conditioning , Daily 
+                                    {/* card-text start here */}
+                                    <p className="card-text">Radisson Noida Hotel has many facilities like Free Parking,
+                                        Free WiFi , Swimming Pool , Bath , Air conditioning , Daily
                                         HouseKeeping , Key card access etc.
+                                        <img src={hotel3} alt="hotel3" />
                                     </p>
+                                    {/* card-text start here */}
                                 </div>
+                                {/* card-body will ends here */}
                             </div>
                         </div>
                     </div>
+                    {/* Row 1 */}
                 </div>
+                {/* content ends here */}
             </div>
         </>
     )
