f311978219e1c1ca17969bf54c2270b17dbda7cd
Nitin1604 <nitinsokhal@gmail.com>
2023-05-28 23:46:31 +0530

    Minor Change to Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -48,10 +48,10 @@ import NavigationComponent from "./Navigation";
                 return setErrorMessage('Name cannot be empty');
             } else if(inputEmail ==='') {
                 return setErrorMessage('Email cannot be empty');
-            } else if(inputAddress ==='') {
-                return setErrorMessage('Addres cannot be empty');
             } else if(inputContact ==='') {
                 return setErrorMessage('Contact cannot be empty');
+            } else if(inputAddress ==='') {
+                return setErrorMessage('Addres cannot be empty');
             } else {
                 setValidationError('')
             }
