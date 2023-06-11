32237463fa88f0d8136d29b5b17dea9b18eae871
Nitin1604 <nitinsokhal@gmail.com>
2023-06-11 17:16:06 +0530

    CODE CHANGE KR DIYA BHAI

--- src/Components/login.js
+++ src/Components/login.js
@@ -60,6 +60,7 @@ const LoginComponent = () => {
         }
         loginApiCall().then(response => {
             navigate("/register");
+            alert('You have been successfully logged in!!')
         });
     }
 
